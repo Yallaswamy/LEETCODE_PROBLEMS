3cde530647bbbdad5090c46b52eb8f50914fa70e
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2024-01-23 22:54:14 +0530

    Time: 283 ms (20.79%) | Memory: 178 MB (13.55%) - LeetSync

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
    int find(string s){
-       unordered_map<char,int>mp;
+       map<char,int>mp;
        for(int i=0;i<s.size();i++){
            mp[s[i]]++;
        }
        return mp.size()==s.size();
    }
 
-   void solve(int ind,vector<string>&arr,string s,int &ans){
+   void solve(int ind,vector<string>&arr,string s,int &maxi){
         
         if(ind==arr.size()){
             return ;
         }
         if(find(s+arr[ind])){
-            ans=max(ans,(int)(s+arr[ind]).size());
-            solve(ind+1,arr,s+arr[ind],ans);
+            maxi=max(maxi,(int)(s+arr[ind]).size());
+            solve(ind+1,arr,s+arr[ind],maxi);
         }
-        solve(ind+1,arr,s,ans);
+        solve(ind+1,arr,s,maxi);
        
     }
     int maxLength(vector<string>& arr) {
         int ind=0;
-        int ans=0;
-        solve(0,arr,"",ans);
-        return ans;
+        int maxi=0;
+        string s="";
+        solve(0,arr,s,maxi);
+        return maxi;
     }
 };
