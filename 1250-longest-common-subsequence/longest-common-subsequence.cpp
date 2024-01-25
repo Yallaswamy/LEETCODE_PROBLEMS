9aa105575bfac8da7781ed5653f31a8908ca3ded
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2024-01-25 16:55:13 +0530

    Time: 74 ms (14.71%) | Memory: 25.8 MB (6.35%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int solve(string &text1, string &text2,int n1,int n2,vector<vector<int>>&dp){
-        if(n1<0) return 0;
-        if(n2<0) return 0;
+    int solve(int ind1,int ind2 ,string &text1, string &text2,int n1,int n2,vector<vector<int>>&dp){
+        if(ind1>n1) return 0;
+        if(ind2>n2) return 0;
         int pick=0;
-        if(dp[n1][n2]!=-1) return dp[n1][n2];
-        if(text1[n1]==text2[n2]){
-            pick=1+solve(text1,text2,n1-1,n2-1,dp);
+        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
+        if(text1[ind1]==text2[ind2]){
+            pick=1+solve(ind1+1,ind2+1,text1,text2,n1,n2,dp);
         }
-        int nonpick=max(solve(text1,text2,n1-1,n2,dp),solve(text1,text2,n1,n2-1,dp));
-        return dp[n1][n2]=max(pick,nonpick);
+        int nonpick=max(solve(ind1+1,ind2,text1,text2,n1,n2,dp),solve(ind1,ind2+1,text1,text2,n1,n2,dp));
+        return dp[ind1][ind2]=max(pick,nonpick);
     }
 
     int longestCommonSubsequence(string text1, string text2) {
         int n1=text1.size(),n2=text2.size();
         vector<vector<int>>dp(n1,vector<int>(n2,-1));
-      
-        return  solve(text1,text2,n1-1,n2-1,dp);
+        int ind0=0,ind1=0;
+        return  solve(ind0,ind1,text1,text2,n1-1,n2-1,dp);
 
     }
 };
