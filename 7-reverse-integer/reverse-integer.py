d09d136b9d64c164365d198a2ecae48ce876129c
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2024-01-31 11:58:52 +0530

    Time: 14 ms (77.24%) | Memory: 11.7 MB (89.85%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        rev=0
+        k=0
+        s=x
+        if(x<0):
+            x=x*-1
+        n=x
+        while(n!=0):
+            k=n%10
+            rev=rev*10+k
+            n=n//10
+        if rev<2**(-31) or rev>2**31:
+            return 0
+
+        elif(s<0):
+            return -(rev)
+        else:
+            return rev
