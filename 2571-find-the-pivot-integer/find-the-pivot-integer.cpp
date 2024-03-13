94373d28b659fab6de9281b42cdaf27f6fb7372f
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2024-03-13 18:54:21 +0530

    Time: 60 ms (5.10%) | Memory: 7.2 MB (73.82%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        if(n==1) return 1;
+        int sum=(n*(n+1))/2;
+        int h=sum/2;
+        int i=1,ele=0,ele1=0,j=n;;
+        while(ele<h and ele1<h){
+            if(ele<=ele1 ){
+                ele+=i;
+                cout<<ele<<i<<" ";
+                i++;
+            }
+            if(i+1==j+1 and ele1==ele ) return i;
+            if(ele1<=ele){
+                ele1+=j;
+                 cout<<ele1<<j<<" ";
+                j--;
+            } 
+           
+        }
+        return -1;
+
+    }
+};
