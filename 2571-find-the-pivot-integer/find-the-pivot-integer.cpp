1d73ab003f56652fa2dfcd0768a2aca66a8965da
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2024-03-13 19:15:53 +0530

    Time: 4 ms (35.26%) | Memory: 7.3 MB (41.86%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -6,20 +6,10 @@ public:
         int h=sum/2;
         int i=1,ele=0,ele1=0,j=n;;
         while(ele<h and ele1<h){
-            if(ele<=ele1 ){
-                ele+=i;
-                cout<<ele<<i<<" ";
-                i++;
-            }
+            if(ele<=ele1 ) ele+=i++;
             if(i==j and ele1==ele ) return i;
-            if(ele1<=ele){
-                ele1+=j;
-                 cout<<ele1<<j<<" ";
-                j--;
-            } 
-           
+            if(ele1<=ele) ele1+=j--;
         }
         return -1;
-
     }
 };
