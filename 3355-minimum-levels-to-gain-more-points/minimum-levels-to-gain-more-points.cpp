c05bf0784f9d3794f9d4141944f4db6396aa2366
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2024-03-31 07:26:24 +0530

    Time: 231 ms (16.67%) | Memory: 316.6 MB (50.00%) - LeetSync

---
+++ 3355-minimum-levels-to-gain-more-points/minimum-levels-to-gain-more-points.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minimumLevels(vector<int>& nums) {
+        int ans=INT_MAX,sum=0,sum1=0,i=0,j=0;
+        vector<int>Dan,Bob;
+        for( i=0;i<nums.size()-1;i++){
+            if(nums[i]==1) sum+=1;
+            else sum-=1;
+            Dan.push_back(sum);
+        }
+        for(j=nums.size()-1;j>0;j--){
+                if(nums[j]==1) sum1+=1;
+                else sum1-=1;
+                Bob.push_back(sum1);
+        }
+        reverse(Bob.begin(),Bob.end());
+        for(int i=0;i<Dan.size();i++){
+            if(Dan[i]>Bob[i]){
+                return i+1;
+            }
+        }
+        return -1;
+    }
+};
