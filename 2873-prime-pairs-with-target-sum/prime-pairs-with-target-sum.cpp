6b39986a775c44b0817b915cb8aca19c922c3690
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2024-04-06 21:40:17 +0530

    Time: 191 ms (93.86%) | Memory: 208.1 MB (32.08%) - LeetSync

--- 2873-prime-pairs-with-target-sum/prime-pairs-with-target-sum.cpp
+++ 2873-prime-pairs-with-target-sum/prime-pairs-with-target-sum.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    bool isprime(int n){
-        if(n<=1) return 0;
+    
+    vector<vector<int>> findPrimePairs(int n) {
+        vector<int>isprime(n+1,1);
+        isprime[0]=0;
+        isprime[1]=0;
         for(int i=2;i*i<=n;i++){
-            if(n%i==0){
-                return 0;
+            if(isprime[i]){
+                for(int j=i*i;j<=n;j+=i){
+                    isprime[j]=0;
+                }
             }
         }
-        return 1;
 
-
-    }
-    vector<vector<int>> findPrimePairs(int n) {
         vector<vector<int>>v;
         for(int i=1;i<=n/2;i++){
-            if(isprime(n-i) and isprime(i)){
+            if(isprime[n-i] and isprime[i]){
                 v.push_back({i,n-i});
             }
         }
-        sort(v.begin(),v.end());
+      //  sort(v.begin(),v.end());
         return v;
     }
 };
