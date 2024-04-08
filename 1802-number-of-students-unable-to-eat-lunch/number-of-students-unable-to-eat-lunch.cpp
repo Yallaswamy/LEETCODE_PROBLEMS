7739a055cbd3fe0c1942cc0722dd53742938bdcd
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2024-04-08 21:11:39 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (79.46%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -3,19 +3,13 @@ public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
         int c=0,c1=0;
         for(int i=0;i<students.size();i++){
-            if(students[i]==1){
-                c+=1;
-            }
-            if(sandwiches[i]==1){
-                c1+=1;
-            }
+            if(students[i]==1) c+=1;
         }
         stack<int>st;
         for(int i=sandwiches.size()-1;i>=0;i--){
             st.push(sandwiches[i]);
         }
         int z=students.size()-c,o=c;
-        cout<<z<<" "<<o;
         while(!st.empty()){
             int ele=st.top();
             if(ele==1 and o>=1){
@@ -27,7 +21,7 @@ public:
                 z--;
             }
             else{
-                return z+o;
+                return st.size();
             }
         }
         return 0;
