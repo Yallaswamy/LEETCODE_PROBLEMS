497a11f2e89a922d27a6c0135195e2cd5a817473
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2024-04-13 21:33:37 +0530

    Time: 38 ms (34.89%) | Memory: 15.5 MB (98.29%) - LeetSync

---
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -0,0 +1,49 @@
+#pragma GCC optimize("O3", "unroll-loops")
+class Solution {
+public:
+     int area(vector<int>&v){  
+        int ans=0;     
+        for(int i=0;i<v.size();i++){
+            int maxi=1;
+            ans=max(ans,v[i]*maxi);
+            for(int j=i-1;j>=0;j--){
+                if(v[j]>=v[i]){
+                    maxi++;
+                }
+                else{
+                    break;
+                }
+            }
+            for(int j=i+1;j<v.size();j++){
+                if(v[j]>=v[i]){
+                    maxi++;
+                }
+                else{
+                    break;
+                }
+            }
+            ans=max(ans,v[i]*maxi);
+        }
+        return ans;
+    }
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        vector<int>v(m,0);
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]=='1'){
+                    v[j]++;
+                }
+                else{
+                    v[j]=0;
+                }
+            }
+            ans=max(ans,area(v));
+        }
+        return ans;
+        //[4 1 3 3 2]
+       
+    }
+};
