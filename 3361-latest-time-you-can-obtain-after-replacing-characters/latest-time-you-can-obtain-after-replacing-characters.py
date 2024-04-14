8dfde9b80abba68790bc2a89572efd8b93ed3189
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2024-04-14 12:27:25 +0530

    Time: 16 ms (100.00%) | Memory: 11.6 MB (100.00%) - LeetSync

--- 3361-latest-time-you-can-obtain-after-replacing-characters/latest-time-you-can-obtain-after-replacing-characters.py
+++ 3361-latest-time-you-can-obtain-after-replacing-characters/latest-time-you-can-obtain-after-replacing-characters.py
@@ -9,23 +9,16 @@ class Solution(object):
             return "11:59"
         if s[0]=='?' and s[1]!='1' and s[1]!='?' and s[1]!='0' :
             ans+='0'
-            print('1')
         if s[0]=='?' and( s[1]=='1' or s[1]=='?' or s[1]=='0'):
             ans+='1'
-            print('2')
         if s[1]=='?' and s[0]=='0':
             ans+='9'
-            print('3')
         if s[1]=='?' and (s[0]=='?' or s[0]=='1'):
             ans+='1'
-            print('4')
         if s[3]=='?':
             ans+='5'
-            print('5')
         if s[4]=='?':
             ans+='9'
-            print('6')
-        print(ans)
         ans1=""
         j=0
         for i in range(5):
