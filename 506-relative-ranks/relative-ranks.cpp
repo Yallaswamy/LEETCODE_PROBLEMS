a81bb0982e0e354ce8bdce694d18dc96f1befab5
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2024-05-08 13:19:23 +0530

    Time: 18 ms (16.66%) | Memory: 16.7 MB (18.00%) - LeetSync

--- 506-relative-ranks/relative-ranks.cpp
+++ 506-relative-ranks/relative-ranks.cpp
@@ -5,24 +5,19 @@ public:
         sort(score.begin(),score.end(),greater<int>());
         map<int,string>mp;   
         vector<string>ans;
-            mp[score[0]]="Gold Medal";
-            if(score.size()>1)
-            mp[score[1]]="Silver Medal";
-            if(score.size()>2)
-            mp[score[2]]="Bronze Medal";
-            int j=4;
-            for(int i=3;i<score.size();i++){
-                mp[score[i]]=to_string(j);
-                j++;
-            }
-            for(auto it:mp){
-                cout<<it.first<<" "<<it.second<<" ";
-            }
-           
-            for(int i=0;i<v.size();i++){
-                ans.push_back(mp[v[i]]);
-            }
-        
+        mp[score[0]]="Gold Medal";
+        if(score.size()>1)
+        mp[score[1]]="Silver Medal";
+        if(score.size()>2)
+        mp[score[2]]="Bronze Medal";
+        int j=4;
+        for(int i=3;i<score.size();i++){
+            mp[score[i]]=to_string(j);
+            j++;
+        }
+        for(int i=0;i<v.size();i++){
+            ans.push_back(mp[v[i]]);
+        }
         return ans;
 
     }
