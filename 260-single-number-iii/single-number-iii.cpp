23e404c010987038399bf015c94e389ac978e574
Manikanta Swamy Yalla <97425775+Yallaswamy@users.noreply.github.com>
2024-05-31 12:25:46 +0530

    Time: 10 ms (38.25%) | Memory: 13.2 MB (33.43%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        vector<int>ans;
+        for(auto it:mp){
+            if(it.second==1) ans.push_back(it.first);
+            
+        }
+        return ans;
+    }
+};
