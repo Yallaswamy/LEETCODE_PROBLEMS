d100b2667212ab0d2147615800c89c78147f8cd2
Manikanta Swamy Yalla <97425775+Yallaswamy@users.noreply.github.com>
2024-07-01 22:03:35 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (65.58%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-   void solve(int ind,int sum,vector<int>&arr,vector<int>&ds,vector<vector<int>>&v)
-   {
-       if(ind>=arr.size()){
-           if(sum==0){
+    void slove(int ind,int sum,vector<int>&ds,vector<vector<int>>&v,vector<int>& arr){
+        if(ind>=arr.size()){
+            if(sum==0){
                 v.push_back(ds);
+                return ;
             }
-             return ;
-       }
-       if(arr[ind]<=sum){
-           ds.push_back(arr[ind]);
-           solve(ind,sum-arr[ind],arr,ds,v);
-           ds.pop_back();
-       }
-       solve(ind+1,sum,arr,ds,v);
-   }
-    vector<vector<int>> combinationSum(vector<int>& arr, int target) {
+         return ;
+        }
+        if(sum-arr[ind]>=0){
+            ds.push_back(arr[ind]);
+            slove(ind,sum-arr[ind],ds,v,arr);
+             ds.pop_back();
+        }
+        slove(ind+1,sum,ds,v,arr);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>>v;
         vector<int>ds;
-        int ind=0;
-        solve(ind,target,arr,ds,v);
+        slove(0,target,ds,v,candidates);
         return v;
     }
 };
