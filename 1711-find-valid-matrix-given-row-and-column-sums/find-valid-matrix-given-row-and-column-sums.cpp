928595abf2fab6d8926fabe4ceebf3c92940f311
Manikanta Swamy Yalla <97425775+Yallaswamy@users.noreply.github.com>
2024-07-20 21:34:02 +0530

    Time: 49 ms (6.32%) | Memory: 36.2 MB (11.51%) - LeetSync

--- 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.cpp
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
-        int n=rowSum.size(),m=colSum.size(),i=0,j=0;
-        vector<vector<int>>ans(n,vector<int>(m,0));
-        while(i<n && j<m){
-            ans[i][j]=min(rowSum[i],colSum[j]);
-            rowSum[i]-=ans[i][j];
-            colSum[j]-=ans[i][j];
-            if(rowSum[i]==0)
-                i++;
-            if(colSum[j]==0)
-                j++;
+        int rows = rowSum.size();
+        int col = colSum.size();
+        vector<vector<int>>ans (rows, vector<int>(col));
+        for (int i = 0 ; i < rows ; i++){
+            for (int j = 0 ; j< col ;j++){
+                ans[i][j] = min(rowSum[i], colSum[j]);
+                rowSum[i] = rowSum[i] - ans[i][j];
+                colSum[j] = colSum[j] - ans[i][j];
+            }
         }
-        return ans;
+        return ans ; 
     }
 };
