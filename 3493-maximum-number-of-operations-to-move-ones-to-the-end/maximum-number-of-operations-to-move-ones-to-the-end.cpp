e6fefd9f5d34acdb9dfaada2b574fe743d8acdb6
Manikanta Swamy Yalla <97425775+Yallaswamy@users.noreply.github.com>
2024-07-21 10:23:15 +0530

    Time: 24 ms (100.00%) | Memory: 17.2 MB (16.67%) - LeetSync

---
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxOperations(string s) {
+        int one=0,end=0,start=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='1'){
+                start=i;
+                break;
+            }
+        }
+        vector<int>v;
+        int f=0;
+        for(int i=start;i<s.size();i++){
+            if(s[i]=='1'){
+                one+=1;
+                f=1;
+            }
+            else if(f==1 and s[i]=='0'){
+                v.push_back(one);
+                f=0;
+            }
+               
+        }
+        int ans=0;
+        for(int i=0;i<v.size();i++){
+            ans+=v[i];
+        }
+        return ans;
+    }
+};
