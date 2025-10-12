dfea91b1747d342211753d5d9347da1d41df5059
Manikanta Swamy Yalla <97425775+Yallaswamy@users.noreply.github.com>
2025-10-12 10:09:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (100.00%) - LeetSync

--- 4052-equal-score-substrings/equal-score-substrings.cpp
+++ 4052-equal-score-substrings/equal-score-substrings.cpp
@@ -3,7 +3,7 @@ public:
     bool scoreBalance(string s) {
         int ans1=0,ans2=0;
         for(int i=0;i<s.size();i++){
-            ans1+=s[i]-'0'-48;
+            ans1+=(s[i]-'0')-48;
             ans2=0;
             for(int j=i+1;j<s.size();j++){
                 ans2+=(s[j]-'0')-48;
